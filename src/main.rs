5f0ba5eac4f14f843ed2ac2bec9c29ae9ccd7d0a
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-09-17 19:14:59 +0200

    feat: create staring point for websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use std::net::SocketAddr;
+
+#[tokio::main] 
+async fn main() {
+    let app = Router::new();
+
+    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
